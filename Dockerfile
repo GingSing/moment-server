5427803c09240f87befac5f8df909f0e3fc6f47c
Gary Yue <garyyue2017@gmail.com>
2024-01-13 19:16:24 -0800

    update app

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Use an official Node runtime as a parent image
 FROM node:18-alpine
 
-# Set the working directory to /app
+# Set the working directory to /
 WORKDIR .
 
 # Copy package.json and package-lock.json to the working directory
@@ -10,7 +10,7 @@ COPY package*.json ./
 # Install app dependencies
 RUN yarn install
 
-# Copy the current directory contents into the container at /app
+# Copy the current directory contents into the container at /
 COPY . .
 
 # Make port 3000 available to the world outside this container
---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const helmet = require("helmet");
+const compression = require("compression");
+const cors = require("cors");
+const httpStatus = require("http-status");
+const routes = require("./routes/v1");
+const ApiError = require("./utils/ApiError");
+const path = require("path");
+// const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// set security HTTP headers
+app.use(helmet());
+
+//parse json request body
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
+
+//parse cookie response
+// app.use(cookieParser());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// gzip compression
+app.use(compression());
+
+const corsOptions = {
+  origin: true,
+  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+  allowedHeaders: "Content-Type",
+  credentials: true,
+};
+
+// enable cors
+app.use(cors(corsOptions));
+app.options("*", cors());
+
+// v1 api routes
+app.use("/v1", routes);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// send 404 response to unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not Found"));
+});
+
+module.exports = app;
