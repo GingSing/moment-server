6c8dabd759a1e882d5b99c10ad9e9ae42f732f4b
Gary Yue <garyyue2017@gmail.com>
2024-01-13 18:57:20 -0800

    add index code

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+let server;
+
+server = app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}.`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.log("Unexpected Error has occurred.", error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// when process is killed
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received");
+  if (server) {
+    server.close();
+  }
+});
